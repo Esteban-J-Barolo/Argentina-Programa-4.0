46e9a949017ffae9e6f14fa46b65f74bf84603f0
Esteban-J-Barolo <estebanjavierbarolo@gmial.com>
2023-03-16 14:17:10 -0300

    Clase 6 integrador1.1

--- src/Integrador/Calculadora.java
+++ src/Integrador/Calculadora.java
@@ -15,20 +15,30 @@ public class Calculadora {
 		System.out.println("  MENU Calculadora\n1- Sumar\n2- Restar\n3- Multiplicar\n4- Dividir\n5- Limpiar\n6- Salir");
 		Scanner scn = new Scanner(System.in);
 		int opc = 0;
+		double resultado;
 		System.out.print("Opción: ");
 		opc = scn.nextInt();
+		System.out.print("Número 1: ");
+		int num1, num2;
+		num1 = scn.nextInt();
+		System.out.print("Número 2: ");
+		num2 = scn.nextInt();
 		switch (opc) {
 		case 1:
-			suma();
+			resultado=suma(num1, num2);
+			System.out.println(num1+" + "+num2+" = "+resultado);
 			break;
 		case 2:
-			resta();
+			resultado=resta(num1, num2);
+			System.out.println(num1+" - "+num2+" = "+resultado);
 			break;
 		case 3:
-			mult();
+			resultado=mult(num1, num2);
+			System.out.println(num1+" x "+num2+" = "+resultado);
 			break;
 		case 4:
-			div();
+			resultado=div(num1, num2);
+			System.out.println(num1+" / "+num2+" = "+resultado);
 			break;
 		case 5:
 			break;
@@ -36,53 +46,26 @@ public class Calculadora {
 			scn.close();
 			seguir = false;
 			break;
+		default:
+			System.out.println("Opción incorrecta");
 		}
 		return seguir;
 	}
 	
-	public static void suma() {
-		System.out.print("Número 1: ");
-		Scanner scn = new Scanner(System.in);
-		int num1, num2;
-		num1 = scn.nextInt();
-		System.out.print("Número 2: ");
-		num2 = scn.nextInt();
-		System.out.println(num1+" + "+num2+" = "+(num1+num2));
-		try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        } catch (Exception e) {
-            System.out.println("nada");
-        }
+	public static double suma(double num1, double num2) {
+		return num1+num2;
 	}
 	
-	public static void resta() {
-		System.out.print("Número 1: ");
-		Scanner scn = new Scanner(System.in);
-		int num1, num2;
-		num1 = scn.nextInt();
-		System.out.print("Número 2: ");
-		num2 = scn.nextInt();
-		System.out.println(num1+" - "+num2+" = "+(num1-num2));
+	public static double resta(double num1, double num2) {
+		return num1-num2;
 	}
 	
-	public static void mult() {
-		System.out.print("Número 1: ");
-		Scanner scn = new Scanner(System.in);
-		int num1, num2;
-		num1 = scn.nextInt();
-		System.out.print("Número 2: ");
-		num2 = scn.nextInt();
-		System.out.println(num1+" x "+num2+" = "+(num1*num2));
+	public static double mult(double num1, double num2) {
+		return num1*num2;
 	}
 	
-	public static void div() {
-		System.out.print("Dividendo: ");
-		Scanner scn = new Scanner(System.in);
-		int num1, num2;
-		num1 = scn.nextInt();
-		System.out.print("Divisor: ");
-		num2 = scn.nextInt();
-		System.out.println(num1+" / "+num2+" = "+(num1/num2));
+	public static double div(double num1, double num2) {
+		return num1/num2;
 	}
 
 }
